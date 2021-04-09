5076371eff49d61488a49e1daa78f6ab0560fe2f
shresthdubey98 <shresthdubey98@gmail.com>
2021-04-09 11:17:02 +0530

    Updated GitIgnore and added UserModel

---
+++ app/src/main/java/info/androidhive/agrosight/User.java
@@ -0,0 +1,87 @@
+package info.androidhive.agrosight;
+import android.content.Context;
+import android.content.SharedPreferences;
+public class User {
+    SharedPreferences sharedPreferences;
+    Context context;
+    private String token;
+    private String userId;
+    private String fName;
+    private String lName;
+    private String email;
+    private String phone;
+
+    public User(Context context) {
+        this.context = context;
+        sharedPreferences = context.getSharedPreferences("userinfo",Context.MODE_PRIVATE);
+    }
+
+    public String getToken() {
+        token = sharedPreferences.getString("token","");
+        return token;
+    }
+
+    public void setToken(String token) {
+        sharedPreferences.edit().putString("token",email).commit();
+        this.token = token;
+    }
+
+    public String getUserId() {
+        userId = sharedPreferences.getString("userId","");
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        sharedPreferences.edit().putString("userId",email).commit();
+        this.userId = userId;
+    }
+
+    public String getfName() {
+        fName = sharedPreferences.getString("fName","");
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        sharedPreferences.edit().putString("fName",email).commit();
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        lName = sharedPreferences.getString("lName","");
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        sharedPreferences.edit().putString("lName",email).commit();
+        this.lName = lName;
+    }
+
+    public String getEmail() {
+        email = sharedPreferences.getString("email","");
+        return email;
+    }
+
+    public void setEmail(String email) {
+        sharedPreferences.edit().putString("email",email).commit();
+        this.email = email;
+    }
+
+    public String getPhone() {
+        phone = sharedPreferences.getString("phone","");
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        sharedPreferences.edit().putString("phone",email).commit();
+        this.phone = phone;
+    }
+
+    public void removeUser(){
+        setEmail("");
+        setToken("");
+        setfName("");
+        setlName("");
+        setUserId("");
+        setPhone("");
+    }
+}
