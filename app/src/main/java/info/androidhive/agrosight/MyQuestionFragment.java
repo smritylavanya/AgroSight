84bd79c3bcbdb7306acb752ef703e427232da3c6
iamgsomya <iamgsomya@gmail.com>
2021-04-10 15:21:33 +0530

    home navigation added

---
+++ app/src/main/java/info/androidhive/agrosight/MyQuestionFragment.java
@@ -0,0 +1,19 @@
+package info.androidhive.agrosight;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MyQuestionFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragement_my_ques,container,false);
+    }
+}
