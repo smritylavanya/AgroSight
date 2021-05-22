2636c8de0048c68e1c034cfef04cbb7c33ec15bb
iamgsomya <iamgsomya@gmail.com>
2021-05-22 18:21:17 +0530

    ui fixes

--- app/src/main/java/info/androidhive/agrosight/HomeActivity.java
+++ app/src/main/java/info/androidhive/agrosight/HomeActivity.java
@@ -9,41 +9,61 @@ import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import me.ibrahimsn.lib.OnItemSelectedListener;
+import me.ibrahimsn.lib.SmoothBottomBar;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_nav);
-        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListner);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentcontainer,new MyQuestionFragment()).commit();
-
-
-
+        SmoothBottomBar bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnItemSelectedListener(navigationItemSelectedListner);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentcontainer,new QuestionAnswerFragment()).commit();
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListner =new BottomNavigationView.OnNavigationItemSelectedListener() {
+    private OnItemSelectedListener navigationItemSelectedListner =new OnItemSelectedListener() {
         @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
+        public boolean onItemSelect(int i) {
+            System.out.println(i);
             Fragment selectedFragment=null;
-            switch (item.getItemId())
+            switch (i)
             {
-                case R.id.Q_A:
+                case 0:
                     selectedFragment=new QuestionAnswerFragment();
                     break;
-                case R.id.My_Question:
+                case 1:
                     selectedFragment=new MyQuestionFragment();
                     break;
-                case R.id.Others:
+                case 2:
                     selectedFragment=new OthersQuestionFragment();
                     break;
-
             }
             getSupportFragmentManager().beginTransaction().replace(R.id.fragmentcontainer,selectedFragment).commit();
             return true;
         }
+
+//        @Override
+//        public boolean onItemSelected(@NonNull MenuItem item) {
+//
+//            Fragment selectedFragment=null;
+//            switch (item.getItemId())
+//            {
+//                case R.id.Q_A:
+//                    selectedFragment=new QuestionAnswerFragment();
+//                    break;
+//                case R.id.My_Question:
+//                    selectedFragment=new MyQuestionFragment();
+//                    break;
+//                case R.id.Others:
+//                    selectedFragment=new OthersQuestionFragment();
+//                    break;
+//
+//            }
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentcontainer,selectedFragment).commit();
+//            return true;
+//        }
 
     };
 }
