004320819358e542bf4f9db688159e53fc8b2a8d
smritylavanya <43835517+smritylavanya@users.noreply.github.com>
2021-06-01 13:42:56 +0530

    Profile Section Updated

--- app/src/main/java/info/androidhive/agrosight/ProfileActivity.java
+++ app/src/main/java/info/androidhive/agrosight/ProfileActivity.java
@@ -3,6 +3,7 @@ package info.androidhive.agrosight;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,15 +11,19 @@ import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
     Button logoutButton;
-    TextView fullName;
+    TextView fullName,email,phonenumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         logoutButton = findViewById(R.id.btn_logout);
         User user = new User(this);
-        fullName = findViewById(R.id.profileFullName);
+        fullName = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+        phonenumber = findViewById(R.id.phonenumber);
         fullName.setText(user.getfName()+" "+user.getlName());
+        email.setText("Email: "+user.getEmail());
+        phonenumber.setText("Phone Number: "+user.getPhone());
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
